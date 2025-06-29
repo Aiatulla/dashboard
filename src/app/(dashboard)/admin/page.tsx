1e074b97540ea46e8238596c3ae82f80f58fb233
Aiatulla <ayatullamidinuulu@gmail.com>
2025-06-29 13:43:56 +0600

    count-chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,22 +1,34 @@
-import UserCard from "@/components/user-card"
+import CountChart from "@/components/count-cart";
+import UserCard from "@/components/user-card";
 
 const AdminPage = () => {
   return (
     <div className="flex flex-col md:flex-row gap-4">
       {/* Left */}
-      <div className=" w-2/3">
-      {/* User Cards */}
-      <div className="flex gap-4 justify-between flex-wrap">
-        <UserCard type="student"/>
-        <UserCard type="teacher"/>
-        <UserCard type="parent"/>
-        <UserCard type="staff"/>
-      </div>
+      <div className=" w-2/3 flex flex-col gap-8">
+        {/* User Cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex flex-col gap-4 lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[350px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[350px]"></div>
+        </div>
+        {/* BOTTOM CHARTS */}
+        <div></div>
       </div>
       {/* Right */}
       <div className="w-1/3">right</div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
