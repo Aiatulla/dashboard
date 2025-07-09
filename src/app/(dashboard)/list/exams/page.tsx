c18e4383dfe7be3036ba72e0691f72fb3585e0fd
Aiatulla <ayatullamidinuulu@gmail.com>
2025-07-09 17:16:02 +0600

    exams list page

---
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -0,0 +1,96 @@
+import Pagination from "@/components/pagination";
+import Table from "@/components/table";
+import TableSearch from "@/components/table-search";
+import { classesData, examsData, role, subjectsData } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+type ExamsData = {
+  id: number;
+  subject: string;
+  class: string;
+  teacher: string;
+  date: string;
+};
+
+const columns = [
+  {
+    header: "Subject",
+    accessor: "subject",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Teacher",
+    accessor: "teacher",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+
+const ExamsListPage = () => {
+  const renderRow = (item: ExamsData) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamePurpleLight"
+    >
+      <td className="flex gap-2 items-center p-3">{item.subject}</td>
+      <td className="hidden md:table-cell ">{item.class}</td>
+      <td className="hidden md:table-cell ">{item.teacher}</td>
+      <td className="hidden md:table-cell ">{item.date}</td>
+      <td>
+        <div className="flex gap-2 items-center">
+          <Link href={`list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/edit.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="bg-white p-4 rounded flex-1 m-4 mt-0">
+      <div className="flex items-center justify-between">
+        <h1 className="font-semibold hidden md:block">All Exams</h1>
+        <div className="flex flex-col md:flex-row gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex self-end gap-2">
+            <button className="bg-lamaYellow rounded-full w-8 h-8 flex items-center justify-center">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="bg-lamaYellow rounded-full w-8 h-8 flex items-center justify-center">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <button className="bg-lamaYellow rounded-full w-8 h-8 flex items-center justify-center">
+                <Image src="/plus.png" alt="" width={14} height={14} />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      <Table columns={columns} renderRow={renderRow} data={examsData} />
+      <Pagination />
+    </div>
+  );
+};
+
+export default ExamsListPage;
